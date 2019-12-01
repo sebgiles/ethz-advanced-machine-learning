05c60c49dbb8593cfa2583e910fa1f5150c60ef4
Sebastian Giles <sgiles@eu-login-19.euler.ethz.ch>
2019-12-01 23:44:05 +0100

    task3 dump after last submission

--- task3/my_utils.py
+++ task3/my_utils.py
@@ -58,3 +58,18 @@ def plot_confusion_matrix(y_true, y_pred, classes,
                     color="white" if cm[i, j] > thresh else "black")
     fig.tight_layout()
     return ax
+
+import seaborn as sns
+
+def corrmap(corrcoef):
+    ax = sns.heatmap(
+        corrcoef, 
+        vmin=-1, vmax=1, center=0,
+        cmap=sns.diverging_palette(20, 220, n=200),
+        square=True
+    )
+    ax.set_xticklabels(
+        ax.get_xticklabels(),
+        rotation=45,
+        horizontalalignment='right'
+    );
